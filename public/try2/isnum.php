cc432f48217e11f7ada968704a734f9bb03d610a
MaebashiEiki <2201031@s.asojuku.ac.jp>
2025-04-15 01:33:47 +0000

    完成

--- public/try2/isnum.php
+++ public/try2/isnum.php
@@ -9,6 +9,13 @@
     <div class="login-box">
       <h2>
 <?php
+  $indata = filter_input(INPUT_GET, 'indata');
+
+  if (!is_string($indata) || trim($indata) === '') {
+      die("indataを入力してください");
+  }else if (mb_ereg('^[0-9]+$', $indata) === false) {
+      die("数字を入力してください。");
+  }
 
 
 echo "入力された数字は： " . $_GET['indata'];
